6151e53229dd25b9a05c1df6a679101ca9269d22
jo3l17 <joelvaldezangeles@gmail.com>
2019-05-23 15:11:22 -0500

    Proyecto finalizado V1

---
+++ precache-manifest.03f45d26dc6e171f1ff23d765d4c1c66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0755b1bf8b8f8fbfe7f9f4e44fd49afe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b01b9807e51133623d6f",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "92625e8f421b478da1a1",
+    "url": "/static/js/2.5ca6e9a6.chunk.js"
+  },
+  {
+    "revision": "b01b9807e51133623d6f",
+    "url": "/static/js/main.9f218a38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
